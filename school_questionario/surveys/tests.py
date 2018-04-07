af3a4ff633727dba3c7c9ac8485e8ada2a24edd4
Marcin Stepniak <stepniak.martin@gmail.com>
2018-04-07 16:26:16 +0200

    slug in survey.name

--- school_questionario/surveys/tests.py
+++ school_questionario/surveys/tests.py
@@ -11,6 +11,9 @@ class SurveysTests(TestCase):
     def test_subject_class(self):
         self.assertTrue(Survey)
 
+    def test_subject_class_unique_name(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main

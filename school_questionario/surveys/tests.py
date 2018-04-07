82382270c495c89269f57227bff2e1911628cdbc
Marcin Stepniak <stepniak.martin@gmail.com>
2018-04-07 15:25:40 +0200

    tests in progress

--- school_questionario/surveys/tests.py
+++ school_questionario/surveys/tests.py
@@ -1,3 +1,16 @@
+import unittest
+
 from django.test import TestCase
 
 # Create your tests here.
+from surveys.models import Survey
+
+
+class SurveysTests(TestCase):
+
+    def test_subject_class(self):
+        self.assertTrue(Survey)
+
+
+if __name__ == '__main__':
+    unittest.main
